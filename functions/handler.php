46255a2868d9a9f20a3932397b92a56a5b5c741e
Ruslan Alfimov <r.alfimov@newcontact.su>
2016-11-24 15:28:27 +0300

    реализовано стандартное тестирование

--- functions/handler.php
+++ functions/handler.php
@@ -6,36 +6,91 @@
  * Time: 21:00
  */
 
+
+/**
+ * Вид ответа сообщения от сервера в формате JSON, после валидации XML.
+ *
+ * Именно такой формат парсится в JS для вывода на страницу с результатами.
+ *
+ *Array
+(
+    [hasErrors] => 1
+    [errorList] => Array
+        (
+            [0] => Array
+                (
+                    [ERROR_TYPE] => FATAL_ERROR
+                    [HEADER] => Fatal error 4:
+                    [MESSAGE] => Start tag expected, '<' not found
+                    [LINE] =>  на строке 1
+                    )
+
+            [1] => Array
+                (
+                    [ERROR_TYPE] => ERROR
+                    [HEADER] => Error 1872:
+                    [MESSAGE] => The document has no document element.
+                    [LINE] =>  на строке -1
+                )
+        )
+)
+ */
+
 switch ($_REQUEST['action']) {
 
     case 'validateXml':
 
-        $xml = $_REQUEST['xml_doc'] ? $_REQUEST['xml_doc'] : null;
-        $xsd = $_REQUEST['xsd_doc'] ? $_REQUEST['xsd_doc'] : null;
+        $xml    = $_REQUEST['xml_doc'] ? $_REQUEST['xml_doc'] : null;
+        $xsd    = $_REQUEST['xsd_doc'] ? $_REQUEST['xsd_doc'] : null;
+        $params = $_REQUEST['test_params'] ? $_REQUEST['test_params'] : null;
 
         if(isset($xml) && isset($xsd)) {
 
-            // Enable user error handling
-            libxml_use_internal_errors(true);
+            foreach ($params as $param) {
+                switch ($param) {
+                    /**
+                     * Стандартный тест валидирования.
+                     */
+                    case 'DEFAULT':
 
-            $xmlDoc = new DOMDocument();
-            $xmlDoc->loadXML($xml);
+                        $data = default_validation($xml, $xsd);
 
-            $has_errors = false;
-            if (!$xmlDoc->schemaValidate($xsd)) {
-                $has_errors = true;
-                $error_body = libxml_display_errors();
+                        print_r(json_encode($data));
+                        die();
+
+                        break;
+                }
             }
-
-            $data = [
-                'hasErrors' => $has_errors,
-                'errorList' => $error_body
-            ];
-
-            print_r(json_encode($data));
-            die();
         }
         break;
+}
+/**
+ * Стандартный механизм валидирования.
+ *
+ * @param $xml
+ * @param $xsd
+ * @return array
+ */
+function default_validation($xml, $xsd) {
+
+    //Включаем информацию об ошибках.
+    libxml_use_internal_errors(true);
+
+    $xmlDoc = new DOMDocument();
+    $xmlDoc->loadXML($xml);
+
+    $has_errors = false;
+    if (!$xmlDoc->schemaValidate($xsd)) {
+        $has_errors = true;
+        $error_body = libxml_display_errors();
+    }
+
+    $data = [
+        'hasErrors' => $has_errors,
+        'errorList' => $error_body
+    ];
+
+    return $data;
 }
 
 
@@ -45,25 +100,34 @@ switch ($_REQUEST['action']) {
  * @param $error Конкретная ошибка.
  * @return string Возвращает информацию об ошибке.
  */
-function libxml_display_error($error)
-{
-    $return = "";
+function libxml_display_error($error) {
+    $return = [];
     switch ($error->level) {
+
         case LIBXML_ERR_WARNING:
-            $return .= "Warning $error->code: ";
+            $return['ERROR_TYPE'] = "WARNING";
+            $return['HEADER'] = "[WARNING] code: $error->code: ";
             break;
+
         case LIBXML_ERR_ERROR:
-            $return .= "Error $error->code: ";
+            $return['ERROR_TYPE'] = "ERROR";
+            $return['HEADER'] = "[ERROR] code: $error->code: ";
             break;
+
         case LIBXML_ERR_FATAL:
-            $return .= "Fatal Error $error->code: ";
+            $return['ERROR_TYPE'] = "FATAL_ERROR";
+            $return['HEADER'] = "[FATAL ERROR] code: $error->code: ";
             break;
+
     }
-    $return .= trim($error->message);
+
+    $return['MESSAGE'] = trim($error->message);
+
     if ($error->file) {
-        $return .= " in $error->file";
+        $return['FILE']= " в файле $error->file";
     }
-    $return .= " on line $error->line";
+
+    $return['LINE']= " на строке $error->line";
 
     return $return;
 }
@@ -74,8 +138,7 @@ function libxml_display_error($error)
  *
  * @return array Возвращается массив с описанием всех ошибок.
  */
-function libxml_display_errors()
-{
+function libxml_display_errors() {
     $errors = libxml_get_errors();
 
     $errArr = [];
